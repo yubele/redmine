30f80f358c32f03197e57b856f7a04c055036584
yusuke abe <yusuke@newsdict.net>
2017-03-05 06:51:58 +0900

    Change home

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 Rails.application.routes.draw do
-  root :to => 'welcome#index', :as => 'home'
+  root :to => 'my#page', :as => 'home'
 
   match 'login', :to => 'account#login', :as => 'signin', :via => [:get, :post]
   match 'logout', :to => 'account#logout', :as => 'signout', :via => [:get, :post]
